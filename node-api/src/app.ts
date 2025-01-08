341cbc210beea6871ad687ba30a06647566a8cb0
rodolfomno <rodolfo_mno@hotmail.com>
2025-01-08 03:13:09 -0300

    feat: create general route

--- node-api/src/app.ts
+++ node-api/src/app.ts
@@ -1,10 +1,17 @@
 import express, { Application } from 'express';
+import { Router, Request, Response } from "express";
 import tasksRoutes from './routes/tasksRoutes';
+
+const router = Router();
 
 const app: Application = express();
 app.use(express.json());
 
 // Rotas
 app.use('/tasks', tasksRoutes);
+
+app.use('/', router.get("/", (req, res) => {
+    return res.json({ message: 'API is running' });
+  }))
 
 export default app;
